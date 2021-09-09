66f8d4d3c52e20beccf9d24c7d42b13c1da78ba7
imaaa34 <oct.imd34@gmail.com>
2021-09-09 18:43:49 +0900

    [Finish] chapter12

---
+++ src/chapter12/Jdbc.java
@@ -0,0 +1,40 @@
+package chapter12;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Jdbc {
+	public void jdbc() {
+		try {
+			Class.forName("org.h2.Driver");
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:h2:~/rpgdb");
+			PreparedStatement pstmt = con.prepareStatement("DELETE FROM MONSTERS WHERE HP <= ? OR NAME =?");
+			pstmt.setInt(1, 10);
+			pstmt.setString(2, "ゾンビ");
+			int r = pstmt.executeUpdate();
+			if(r != 0) {
+				System.out.println(r + "件のモンスターを削除しました。");
+			} else {
+				System.out.println("該当するモンスターはいませんでした。");
+			}
+			pstmt.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(con != null) {
+				try {
+					con.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
